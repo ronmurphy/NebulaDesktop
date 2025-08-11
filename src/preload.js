8dcc0d9b1fcf42bc8871c89f55af1f3f0c35ede0
RonMurphy <solo.dev@gmail.com>
2025-08-11 08:02:01 -0400

    update preload

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,56 @@
-// Simple preload script
-console.log('Preload script loaded successfully');
+// Merging the best from both versions
+const { contextBridge, ipcRenderer } = require('electron');
+const path = require('path');
+
+contextBridge.exposeInMainWorld('nebula', {
+    // System APIs (from WME1's completeness)
+    system: {
+        platform: process.platform,
+        shutdown: () => ipcRenderer.invoke('system:shutdown'),
+        reboot: () => ipcRenderer.invoke('system:reboot'),
+        logout: () => ipcRenderer.invoke('system:logout'),
+    },
+    
+    // Window Management (from EWM2's tab system + WME1's dragging)
+    windows: {
+        create: (options) => ipcRenderer.invoke('window:create', options),
+        close: (id) => ipcRenderer.send('window:close', id),
+        minimize: (id) => ipcRenderer.send('window:minimize', id),
+        maximize: (id) => ipcRenderer.send('window:maximize', id),
+        focus: (id) => ipcRenderer.send('window:focus', id)
+    },
+    
+    // File System (from WME1)
+    fs: {
+        readDir: (path) => ipcRenderer.invoke('fs:readdir', path),
+        readFile: (path) => ipcRenderer.invoke('fs:readfile', path),
+        writeFile: (path, data) => ipcRenderer.invoke('fs:writefile', path, data),
+        getHomeDir: () => ipcRenderer.invoke('fs:homedir')
+    },
+    
+    // Browser features (from EWM2)
+    browser: {
+        createTab: (url) => ipcRenderer.invoke('browser:newtab', url),
+        navigate: (tabId, url) => ipcRenderer.send('browser:navigate', tabId, url),
+        back: (tabId) => ipcRenderer.send('browser:back', tabId),
+        forward: (tabId) => ipcRenderer.send('browser:forward', tabId),
+        refresh: (tabId) => ipcRenderer.send('browser:refresh', tabId)
+    },
+    
+    // Events
+    on: (channel, callback) => {
+        const validChannels = [
+            'window-created', 
+            'window-closed', 
+            'file-changed',
+            'app-installed'
+        ];
+        if (validChannels.includes(channel)) {
+            ipcRenderer.on(channel, (event, ...args) => callback(...args));
+        }
+    },
+    
+    removeListener: (channel, callback) => {
+        ipcRenderer.removeListener(channel, callback);
+    }
+});
